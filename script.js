b44443ef0018d400196e340f24cea92e7367c85a
Protocol1228 <43751555+Protocol1228@users.noreply.github.com>
2019-01-14 10:06:30 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$(".AD").click(function(){
+ $("#Dumbledore").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$("      ").click(function(){
+ 
+});
+
+
+
+//Spell #3
+
+$("      ").click(function(){
+ 
+});
+
+
+
